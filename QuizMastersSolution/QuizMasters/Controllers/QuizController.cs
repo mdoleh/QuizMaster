adbef45380eac2f01c10f9903ab77487f0e0e1ba
Mohammad Doleh <mdoleh@kent.edu>
2014-03-29 06:28:36 -0400

    Working Project
    Can upload and run quizzes

---
+++ QuizMastersSolution/QuizMasters/Controllers/QuizController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizMasters.Controllers
+{
+    public class QuizController : Controller
+    {
+        public ActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
