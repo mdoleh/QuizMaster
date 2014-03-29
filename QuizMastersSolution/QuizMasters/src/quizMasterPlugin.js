9fb1927da3eeb67079f8b2cebf6cb11a9ec69dcf
Mohammad Doleh <mdoleh@kent.edu>
2014-03-29 08:10:51 -0400

    Refactored code
    new service for reading files
    service in its own file
    plugin in its own file

---
+++ QuizMastersSolution/QuizMasters/src/quizMasterPlugin.js
@@ -0,0 +1,105 @@
+﻿(function ($, quizService) {
+    $.fn.quizMaster = function () {
+        // UI elements
+        var $seeAnswerButton = $("#see-answer"),
+            $nextCardButton = $("#next-card"),
+            $restartButton = $("#restart-quiz"),
+            $uploadButton = $("#upload-file"),
+            $quizFile = $("#quiz-file"),
+            $card = $("div > #card"),
+            $uploadFileContainer = $("#upload-file-container"),
+            $quizSection = $("#quiz-section");
+
+        // Global variables
+        var cardSet = [],
+            currentCard,
+            backupCardSet;
+
+        // Button click detection
+        $seeAnswerButton.click(seeAnswer);
+        $nextCardButton.click(nextCard);
+        $restartButton.click(restartQuiz);
+        $uploadButton.click(readFiles);
+
+        // Key press detection
+        $(document).on("keyup", function (event) {
+            if (event.keyCode === utils.keyCodes.spacebar) {
+                if (!$seeAnswerButton.hasClass("hidden")) {
+                    seeAnswer();
+                } else if (!$nextCardButton.hasClass("hidden")) {
+                    nextCard();
+                }
+            }
+        });
+
+        // Helper functions
+        function seeAnswer() {
+            if (currentCard) {
+                showNextCardButton();
+                $card.html(currentCard.answer);
+            } else {
+                showRestartButton();
+            }
+        }
+
+        function nextCard() {
+            currentCard = cardSet.pop();
+            if (currentCard) {
+                showSeeAnswerButton();
+                $card.html(currentCard.question);
+            } else {
+                showRestartButton();
+            }
+        }
+
+        function showRestartButton() {
+            $nextCardButton.addClass("hidden");
+            $seeAnswerButton.addClass("hidden");
+            $restartButton.removeClass("hidden");
+        }
+
+        function showSeeAnswerButton() {
+            $seeAnswerButton.removeClass("hidden");
+            $nextCardButton.addClass("hidden");
+            $restartButton.addClass("hidden");
+        }
+
+        function showNextCardButton() {
+            $seeAnswerButton.addClass("hidden");
+            $nextCardButton.removeClass("hidden");
+            $restartButton.addClass("hidden");
+        }
+
+        function restartQuiz() {
+            cardSet = backupCardSet.slice(0);
+            startQuiz();
+            showSeeAnswerButton();
+        }
+
+        function startQuiz() {
+            $restartButton.blur();
+            cardSet = cardSet.shuffle();
+            currentCard = cardSet.pop();
+            $card.html(currentCard.question);
+        }
+
+        function readFiles() {
+            quizService.readFiles($quizFile, readFilesCallback);
+        }
+
+        function readFilesCallback(data) {
+            cardSet = data;
+            if (cardSet) {
+                backupCardSet = cardSet.slice(0);
+            } else {
+                return;
+            }
+
+            $uploadFileContainer.addClass("hidden");
+            $quizSection.removeClass("hidden");
+            startQuiz();
+        }
+
+        return this;
+    }
+})(jQuery, utils.quizService);
---
+++ QuizMastersSolution/QuizMasters/src/quizService.js
@@ -0,0 +1,54 @@
+﻿window.utils = window.utils || {};
+window.utils.quizService = (function quizService() {
+
+    function arrayToObjectArray(array1, array2) {
+        // Convert arrays into cardSet object array
+        var objectArray = [];
+        for (var ii = 0; ii < array1.length; ++ii) {
+            objectArray.push({ question: array1[ii], answer: array2[ii] });
+        }
+        return objectArray;
+    }
+
+    return {
+        readFiles: function ($quizFile, callback) {
+            if (!$quizFile.val()) {
+                return
+            } else if ($quizFile[0].files.length !== 2) {
+                return
+            }
+            var reader = new FileReader(),
+                content,
+                questionsArray,
+                answersArray,
+                file1, file2;
+
+            reader.onload = function (event1) {
+                content = event1.target.result;
+                questionsArray = content.split("\n");
+                questionsArray = questionsArray.slice(0, questionsArray.length - 2);
+
+                reader.onload = function (event2) {
+                    content = event2.target.result;
+                    answersArray = content.split("\n");
+                    answersArray = answersArray.slice(0, answersArray.length - 2);
+
+                    $quizFile[0].value = "";
+                    callback(arrayToObjectArray(questionsArray, answersArray));
+                }
+
+                if ($quizFile[0].files[0].name.toLowerCase().contains("answers")) {
+                    reader.readAsText($quizFile[0].files[0]);
+                } else {
+                    reader.readAsText($quizFile[0].files[1]);
+                }
+            }
+
+            if ($quizFile[0].files[0].name.toLowerCase().contains("questions")) {
+                reader.readAsText($quizFile[0].files[0]);
+            } else {
+                reader.readAsText($quizFile[0].files[1]);
+            }
+        }
+    }
+})();
